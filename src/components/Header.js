64d81daef884839f4479673ba1d86ac1e9fd3798
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-04-29 18:29:32 +0530

    making differect components file

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+    return (
+      <div className="header">
+        <div className="logo-container">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-HJgYuO5ggkAinuuO8aof4vKV1ECmgi7iuQ&s"
+            alt="logo"
+            className="logo"
+          />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  
