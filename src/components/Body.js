b0a5aee442cc0c71a99818952775500312b8be34
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-05-03 01:26:20 +0530

    fetch from API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,21 @@
 import RestaurantCard from "./RestarantCard";
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import resListData from "../utils/mockData";
 
 const Body = () => {
   const [resList, setResList] = useState(resListData);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4209228&lng=77.5267391&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const json = await data.json();
+    console.log(json);
+    setResList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
 
   return (
     <div className="body">
