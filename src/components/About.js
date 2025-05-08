a1fbbcf1b718d00560c5d3d705ca80d7f7fc5089
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-05-08 18:39:36 +0530

    i Router dom, adding about, contact, error pages

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <h2>This is food ordering App. Let's make your best Cart ever
+                with us.</h2>
+        </div>
+    )
+}
+
+export default About;
