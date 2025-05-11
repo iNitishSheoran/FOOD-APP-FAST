c1212a5511566935ccbdcfe2ff5d208c7f069e8d
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-05-12 00:01:06 +0530

    UserCard

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import User from "./User";
+// import User from "./User";
 import UserClass from "./UserClass";
 
 const About = () => {
--- src/components/User.js
+++ src/components/User.js
@@ -1,18 +1,18 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-const user = ({name}) => {
-    const [count] = useState(0);
-    const [count2] = useState(1);
-    return (
-        <div className="user-card">
+// const user = ({name}) => {
+//     const [count] = useState(0);
+//     const [count2] = useState(1);
+//     return (
+//         <div className="user-card">
 
-        <h1>Count = {count}</h1>
-        <h1>Count2 = {count2}</h1>
+//         <h1>Count = {count}</h1>
+//         <h1>Count2 = {count2}</h1>
 
-        <h2>Name: {name}</h2>
-        <h3>Location: Haryana</h3>
-        <h3>Contact: @nitish_sheoran</h3>
-        </div>
-    );
-};
-export default user;
+//         <h2>Name: {name}</h2>
+//         <h3>Location: Haryana</h3>
+//         <h3>Contact: @nitish_sheoran</h3>
+//         </div>
+//     );
+// };
+// export default user;
