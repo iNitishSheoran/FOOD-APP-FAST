0e671868ed52e8bcf3bd5fe705e4ecafca365f7d
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-05-21 02:03:37 +0530

    Your commit message

--- src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-
-export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-HJgYuO5ggkAinuuO8aof4vKV1ECmgi7iuQ&s";
-
-export const MENU_API_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.4209228&lng=77.5267391&restaurantId=";
