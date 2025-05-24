1528dc1bb5d9a3f953d72f683cfb265af9069694
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-05-24 19:44:06 +0530

    lazy loading

---
+++ src/components/Grocery.js
@@ -0,0 +1,9 @@
+const Grocery = () => {
+    return (
+        <h1>
+            {"Grocery Store is under construction, please check back later"}
+        </h1>
+    );
+};
+
+export default Grocery;
